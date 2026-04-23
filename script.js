3cdae7ada69c3a67ed1070378a3c6ec7ae3f6fdd
Erick Yoakum <46612639+UnderseaEagle1@users.noreply.github.com>
2026-04-23 00:55:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function tn_calc(primer, num_changed_bases) {
+    let filtered = primer.toLowerCase().replace(/\s/g, "");
+
+    if (filtered.length === 0) {
+        return "Error: Primer is empty";
+    }
+
+    let percent_mismatch = (num_changed_bases / filtered.length) * 100;
+
+    let gc_count = (filtered.match(/[gc]/g) || []).length;
+    let percent_gc = (gc_count / filtered.length) * 100;
+
+    let tn_value = 81.5 + (0.41 * percent_gc) - (675 / filtered.length) - percent_mismatch;
+
+    return `Percent GC: ${percent_gc.toFixed(2)}%\nTn Value: ${tn_value.toFixed(2)}`;
+}
+
+
+function runCalculation() {
+    let primer = document.getElementById("primer").value;
+    let changes = document.getElementById("changes").value;
+
+    let output = document.getElementById("output");
+
+    if (isNaN(changes) || changes === "") {
+        output.innerText = "Error: Changed bases must be an integer.";
+        return;
+    }
+
+    let tn = tn_calc(primer, parseInt(changes));
+    output.innerText = tn;
+}
